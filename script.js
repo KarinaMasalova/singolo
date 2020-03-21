6877c4da6fed476f6dedc7e682362a2f8b5344ff
Karina Masalova <masalova.karina@mail.ru>
2020-03-21 18:41:30 +0300

    fead: add close-btn event

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ const PORTFOLIO_TABS = document.querySelectorAll('.portfolio-button');
 const PORTFOLIO_PICTURES = document.querySelector('.portfolio-projects-container');
 
 const SUBMIT_BUTTON = document.getElementById('submit');
+const CLOSE_BTN = document.querySelector('.message-block-btn');
 
 /*menu anchors*/
 MENU.addEventListener('click', function (event) {
@@ -37,11 +38,11 @@ window.addEventListener('scroll', () => {
         MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
         document.getElementById('services-scroll').classList.add('active');
     }
-    if(window.scrollY >= PORTFOLIO.offsetTop - HEADER.offsetHeight) {
+    if(window.scrollY >= PORTFOLIO.offsetTop - HEADER.offsetHeight && window.scrollY < ABOUT_US.offsetTop - HEADER.offsetHeight) {
         MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
         document.getElementById('portfolio-scroll').classList.add('active');
     }
-    if(window.scrollY >= ABOUT_US.offsetTop - HEADER.offsetHeight) {
+    if(window.scrollY >= ABOUT_US.offsetTop - HEADER.offsetHeight && window.scrollY < CONTACT.offsetTop - HEADER.offsetHeight) {
         MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
         document.getElementById('about-scroll').classList.add('active');
     }
@@ -135,7 +136,7 @@ SUBMIT_BUTTON.addEventListener('click', (event) => {
         const subject = document.getElementById('subject').value.toString();
         const description = document.getElementById('description').value.toString();
         document.getElementById('first-result').innerText = 'Письмо отправлено';
-        
+
         if(subject !== '') {
             document.getElementById('second-result').innerText = 'Тема: ' + subject;
         } else {
@@ -147,8 +148,11 @@ SUBMIT_BUTTON.addEventListener('click', (event) => {
         } else {
             document.getElementById('third-result').innerText = 'Без описания';
         }
-
+        
         document.getElementById('message-block').classList.remove('hidden');
-        document.getElementById('feedback-form').reset();
     }
+});
+
+CLOSE_BTN.addEventListener("click", () => {
+    document.getElementById('message-block').add('hidden');
 });
